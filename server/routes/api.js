1b41557966ff8fdaef268705b54dfe16fbe0b5af
didietsuryadi <didietsuryadi>
2017-03-26 05:50:32 +0700

    create-route

---
+++ server/routes/api.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var router = express.Router();
+var userController = require('../controllers/userController')
+var forumController = require('../controllers/forumController')
+var commentController = require('../controllers/commentController')
+var voteController = require('../controllers/voteController')
+
+//User Router
+router.get('/user', userController.findAllUser);
+
+router.get('/user/:id', userController.findUserById);
+
+router.post('/user', userController.createUser);
+
+router.put('/user/:id', userController.updateUser);
+
+router.delete('/user/:id', userController.deleteUser);
+
+
+//Forum router
+router.get('/forum', forumController.findAllForum);
+
+router.get('/forum/:id', forumController.findForumById);
+
+router.post('/forum', forumController.createForum);
+
+router.put('/forum/:id', forumController.updateForum);
+
+router.delete('/forum/:id', forumController.deleteForum);
+
+//Comment Router
+router.get('/comment', commentController.findAllComment);
+
+router.get('/comment/:id', commentController.findCommentById);
+
+router.post('/comment', commentController.createComment);
+
+router.put('/comment/:id', commentController.updateComment);
+
+router.delete('/comment/:id', commentController.deleteComment);
+
+//VOte Router
+router.post('/upvote', voteController.upVote);
+
+router.post('/downvote', voteController.downVote);
+
+module.exports = router;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var auth = require('../helper/auth')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/signin', auth.signIn)
+
+router.post('/register', auth.signUp)
+
+router.post('/verify', auth.verifyToken)
+
 
 module.exports = router;
